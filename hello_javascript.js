eee1d06a1aea6002e62c3401901b26d3bd298d06
rabiiinnn <rabinadhikari148@gmail.com>
2025-04-11 23:21:30 +0545

    add more basic

--- hello_javascript.js
+++ hello_javascript.js
@@ -1,2 +1,31 @@
 console.log("hello java script,this is my first programme in java script");
 console.log("i will complete this course in this sem");
+
+//variables
+name="Rabin Adhikari";
+console.log(name);
+age=21;
+salary=15000;
+console.log(age);
+console.log("my name is ",name);
+console.log("MY age is ",age);
+console.log("my salary is ",salary);
+
+// defining the variables 
+let a=20;
+console.log(a);
+
+const pi=3.1416;
+let r=6;
+A=2*pi*r;
+console.log("area of cricle is",A);
+
+//
+const student ={
+    name:"rabin adhikari",
+    class:12,
+    roll_no:339,
+    cgpa:3.5,
+    college:"xavier international college"
+}
+console.log(student);
